242c3218af4b9f0c66efd88697151f933ed7553f
kirubel tabu <kirubelderessa6@gmail.com>
2022-04-11 23:06:29 +0300

    added getAll for ObjectType

---
+++ Application/ObjectTypeLists/Queries/GetObjectTypesQuery/GetObjectTypesQuery.cs
@@ -0,0 +1,49 @@
+﻿using Application.Common.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.ObjectTypeLists.Queries.GetObjectTypesQuery
+{
+    public class GetObjectTypesQuery : IRequest<ObjectTypeList>
+    {
+
+    }
+
+    public class GetObjectTypesQueryHandler : IRequestHandler<GetObjectTypesQuery, ObjectTypeList>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetObjectTypesQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+        public async Task<ObjectTypeList> Handle(GetObjectTypesQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return new ObjectTypeList
+                {
+                    Lists = await _context.ObjectTypes
+                    .ProjectTo<ObjectTypeListDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync(cancellationToken)
+                };
+            }
+            catch (ArgumentNullException)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Application/ObjectTypeLists/Queries/GetObjectTypesQuery/ObjectTypeList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ObjectTypeLists.Queries.GetObjectTypesQuery
+{
+    public class ObjectTypeList
+    {
+        public IList<ObjectTypeListDto> Lists { get; set; } = new List<ObjectTypeListDto>();
+    }
+}
